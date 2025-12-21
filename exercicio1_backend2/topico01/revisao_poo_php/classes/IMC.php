43fd4085e468f09f12339e7b4f075123809984d7
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-12-21 12:33:52 -0300

    fix: Não implementou as funções estáticas calc e classifica.

--- exercicio1_backend2/topico01/revisao_poo_php/classes/IMC.php
+++ exercicio1_backend2/topico01/revisao_poo_php/classes/IMC.php
@@ -10,10 +10,12 @@ class IMC{
 
     public static function calc(Pessoa $objPessoa){
         echo "Calculando o IMC de $objPessoa->nome\n";
+        //Calcular o IMC
         return;
     }
 
     public static function classifica(Pessoa $objPessoa){
+        // Retornar uma string com a classificação da pessoa.
         return self::calc($objPessoa);
     }
 }
